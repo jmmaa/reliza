2b49149b1a6df902812960e528c568f098ec6cb5
arkhon7 <reviuy9@gmail.com>
2024-02-02 14:45:57 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,233 +1,69 @@
-// base attack speed
+type Status = {};
 
-export const calculateTwoHandedSwordBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  STR: number
-) => 50 + level + 2 * AGI + (AGI + STR - 1) / 5;
+type Prettify<T> = { [K in keyof T]: T[K] } & {}; // idk how does this work
 
-export const calculateDualWieldBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  STR: number
-) => 100 + level + 4 * AGI + (AGI + STR - 1) / 5;
+type BaseSTR = (
+  value: number
+) => <T extends Status>(status: T) => Prettify<T & { STR: number }>;
 
-export const calculateBowBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  DEX: number
-) => 75 + level + 3 * AGI + (AGI + 2 * DEX - 1) / 10;
-
-export const calculateBowgunBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  DEX: number
-) => 30 + level + 2.2 * AGI + 0.2 * DEX;
-
-export const calculateStaffBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  INT: number
-) => 60 + level + AGI + (4 * AGI + INT - 1) / 5;
-
-export const calculateMagicDeviceBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  INT: number
-) => 90 + level + 4 * AGI + (INT - 1) / 5;
-
-export const calculateKnuckleBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  STR: number,
-  DEX: number
-) => 120 + level + 4.6 * AGI + DEX / 10 + STR / 10;
-
-export const calculateKatanaBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  STR: number
-) => 200 + level + 3.9 * AGI + 0.3 * STR;
-
-export const calculateHalberdBaseAttackSpeed = (
-  level: number,
-  AGI: number,
-  STR: number
-) => 25 + level + 3.5 * AGI + 0.2 * STR;
-
-export const calculateBareHandBaseAttackSpeed = (
-  level: number,
-  AGI: number
-) => 1000 + level + 9.6 * AGI;
-
-export const calculateActionTimeReduction = (attackSpeed: number) => {
-  const raw = Math.floor((attackSpeed - 1000) / 180);
-
-  const effective = raw > 50 ? 50 : raw;
-
-  return effective / 100;
+const STR: BaseSTR = (value: number) => {
+  return (status) => {
+    return { ...status, STR: value };
+  };
 };
 
-// max hp
+type BaseAGI = (
+  value: number
+) => <T extends Status>(status: T) => Prettify<T & { AGI: number }>;
 
-export const calculateBaseMaxHP = (level: number, VIT: number) =>
-  93 + ((VIT + 22.4) * level) / 3;
-
-// max mp
-
-export const calculateBaseMaxMP = (
-  level: number,
-  INT: number,
-  TEC: number
-) =>
-  TEC > 0 ? 100 + level + INT / 10 + (TEC - 1) : 100 + level + INT / 10;
-
-// ailment resistance
-
-export const calculateBaseAilmentResistance = (MTL: number) => MTL / 3.4;
-
-// critical rate
-
-export const calculateBaseCriticalRate = (CRT: number) => 25 + CRT / 3.4;
-
-// critical damage
-
-export const calculateBaseCriticalDamage = (AGI: number, STR: number) =>
-  STR >= AGI ? 150 + STR / 5 : 150 + (STR + AGI) / 10;
-
-// cast speed
-
-export const calculateBaseCastSpeed = (
-  level: number,
-  AGI: number,
-  DEX: number
-) => level + (1.16 * AGI + 2.94 * DEX);
-
-export const calculateCastingTimeReduction = (castSpeed: number) =>
-  castSpeed > 1000
-    ? (((castSpeed - 1000) / 90) * 0.5 + 50) / 100
-    : castSpeed / 20 / 100;
-
-// ampr
-
-export const calculateAMPR = (maxMP: number) => 10 + maxMP / 100;
-
-// weapon attack
-
-export const calculateWeaponAttackRefinementBonus = (
-  refinementValue: number,
-  baseWeaponAttack: number
-) => {
-  const percent = refinementValue ** 2 / 100;
-
-  const flat = refinementValue;
-
-  const refinementBonus = Math.floor(baseWeaponAttack * percent + flat);
-
-  return refinementBonus;
+const AGI: BaseAGI = (value: number) => {
+  return (status) => {
+    return { ...status, AGI: value };
+  };
 };
 
-export const calculateSubWeaponAttackRefinementBonus = (
-  refinementValue: number,
-  baseSubWeaponAttack: number
-) => {
-  /*
+type BaseDEX = (
+  value: number
+) => <T extends Status>(status: T) => Prettify<T & { DEX: number }>;
 
-  the actual calculation based on phantom library is:
-
-  Sub Weapon ATK = Attack value of subhand sword * 
-  (1 + Weapon ATK%/100 + refine of weapon²/200) + refine of weapon + flat Weapon ATK
-
-  however it seems like its an inconsistency in the coryn.club example, imma search more on this.
-
-  */
-  const percent = refinementValue ** 2 / 200;
-
-  const flat = refinementValue;
-
-  return Math.floor(baseSubWeaponAttack * percent + flat);
+const DEX: BaseDEX = (value: number) => {
+  return (status) => {
+    return { ...status, DEX: value };
+  };
 };
 
-// stability
+type BaseCriticalDamage = <
+  T extends Status & { AGI: number; STR: number }
+>(
+  status: T
+) => Prettify<
+  T & {
+    baseCriticalDamage: number;
+  }
+>;
 
-export const calculateOneHandedSwordStability = (
-  weaponStability: number,
-  STR: number,
-  DEX: number
-) => weaponStability + (STR + 3 * DEX) / 40;
+const baseCriticalDamage: BaseCriticalDamage = (status) => {
+  const STR = status.STR;
 
-export const calculateTwoHandedSwordStability = (
-  weaponStability: number,
-  DEX: number
-) => weaponStability + DEX / 10;
+  const AGI = status.AGI;
 
-// essentially the same to ohs stab
-export const calculateDualWieldStability = (
-  weaponStability: number,
-  STR: number,
-  DEX: number
-) => weaponStability + (STR + 3 * DEX) / 40;
+  const result = STR >= AGI ? 150 + STR / 5 : 150 + (STR + AGI) / 10;
 
-export const calculateDualWieldSubStability = (
-  subWeaponStability: number,
-  STR: number,
-  AGI: number
-) => subWeaponStability / 2 + (3 * STR + 2 * AGI) / 50;
+  return { ...status, baseCriticalDamage: result };
+};
 
-export const calculateBowStability = (
-  weaponStability: number,
-  STR: number,
-  DEX: number
-) => weaponStability + (STR + DEX) / 20;
+const stat: Status = {};
 
-export const calculateBowgunStability = (
-  weaponStability: number,
-  STR: number
-) => weaponStability + STR / 20;
+const agiStat = AGI(9);
+const dexStat = DEX(247);
+const strStat = STR(465);
 
-export const calculateStaffStability = (
-  weaponStability: number,
-  STR: number,
-  DEX: number
-) => weaponStability + STR / 20;
+const withAgi = agiStat(stat);
+const withDex = dexStat(withAgi);
+const withStr = strStat(withDex);
 
-export const calculateMagicDeviceStability = (
-  weaponStability: number,
-  DEX: number
-) => weaponStability + DEX / 10;
+const withbcdmg = baseCriticalDamage(withStr);
 
-export const calculateKnuckleStability = (
-  weaponStability: number,
-  DEX: number
-) => weaponStability + DEX / 40;
+console.log(withStr.AGI, withStr.DEX, withStr.STR);
 
-export const calculateHalberdStability = (
-  weaponStability: number,
-  STR: number,
-  DEX: number
-) => weaponStability + (STR + DEX) / 20;
-
-export const calculateKatanaStability = (
-  weaponStability: number,
-  STR: number,
-  DEX: number
-) => weaponStability + (3 * STR + DEX) / 40;
-
-export const calculateBareHandStability = (
-  weaponStability: number,
-  DEX: number
-) => weaponStability + DEX / 3;
-
-export const calculateMagicStability = (stability: number) =>
-  (100 + stability) / 2;
-
-// drop rate
-export const calculateBaseDropRate = (LUK: number) => Math.floor(LUK / 5);
-
-// ATK
-
-// scratch
-
-const cspd = calculateCastingTimeReduction(6380);
-
-console.log(cspd);
+console.log(withbcdmg.baseCriticalDamage);
