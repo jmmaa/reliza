ef1b408a416d971ee383cae6bf4956a1e71d004d
arkhon7 <reviuy9@gmail.com>
2025-03-18 16:42:11 +0800

    docs: just some reminder

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export {
   type Stat,
   type StatMapBuilder,
 } from "./stats/types";
+
+// BIG TASK:
+// REDO THE STATCALC CONFIG, FLATTEN IT CUZ IT SEEMS LIKE MERGICIAN IS FAILING TO MERGE SOME DATA
